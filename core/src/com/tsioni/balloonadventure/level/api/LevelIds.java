96aac9b7bcd93e0d930009dfd30b3818b07ad1c4
Gavriel Tsioni <gtsioni@gmail.com>
2019-01-06 14:57:38 -0800

    Added a second test level

--- core/src/com/tsioni/balloonadventure/level/api/LevelIds.java
+++ core/src/com/tsioni/balloonadventure/level/api/LevelIds.java
@@ -3,4 +3,5 @@ package com.tsioni.balloonadventure.level.api;
 public class LevelIds
 {
     public static final LevelId TEST_1 = new LevelId("test1");
+    public static final LevelId TEST_2 = new LevelId("test2");
 }
